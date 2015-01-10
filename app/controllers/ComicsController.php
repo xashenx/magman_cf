cb35e90d975fbba970a4ccec243c9e016186e271
Fabrizio Zeni <fabrizio.zeni@gmail.com>
2015-01-10 21:57:46 +0100

    validator js

--- app/controllers/ComicsController.php
+++ app/controllers/ComicsController.php
@@ -1,105 +1,124 @@
 <?php
-class ComicsController extends BaseController {
 
-	public function create() {
-		$series_id = Input::get('series_id');
-		$comic = new Comic;
-		$comic -> name = Input::get('name');
-		$comic -> number = Input::get('number');
-		$comic -> price = Input::get('price');
-		$comic -> series_id = $series_id;
-		$comic -> available = Input::get('available');
-		$comic -> save();
-		$this -> updateComicUser($series_id, $comic);
-		return Redirect::to('series/' . $series_id);
-	}
+class ComicsController extends BaseController
+{
 
-	public function update() {
-		$id = Input::get('id');
-		$comic = Comic::find($id);
-		$comic -> name = Input::get('name');
-		$comic -> number = Input::get('number');
-		$comic -> available = Input::get('available');
-		$new_price = Input::get('price');
-		if ($new_price != $comic -> price) {
-			$comic -> price = $new_price;
-			DB::update('update comic_user set price = ' . $new_price . ' where comic_id = ' . $id . ' and state_id < 3');
-		}
-		if (Input::get('active'))
-			$comic -> active = 1;
-		else {
-			$comic -> active = 0;
-			DB::update('update comic_user set active = 0 where comic_id = ' . $id);
-		}
-		$comic -> save();
-		$return = Input::get('return');
+    public function create()
+    {
+        $new = Input::all();
+        $series_id = Input::get('series_id');
+        $comic = new Comic;
+        $comic->name = Input::get('name');
+        $comic->number = Input::get('number');
+        $comic->price = Input::get('price');
+        $comic->series_id = $series_id;
+        $comic->available = Input::get('available');
+        if ($comic->validate($new)) {
+            $comic->save();
+            $this->updateComicUser($series_id, $comic);
+            return Redirect::to('series/' . $series_id);
+        } else {
+            $errors = $comic->errors();
+//            echo $errors;
+            return Redirect::to('series/' . $series_id)->withErrors($errors);
+        }
+    }
 
-		if ($return == 'comics')
-			return Redirect::to('comics/' . $id);
-		elseif ($return == 'series')
-			return Redirect::to('series/' . $comic -> series_id . '/' . $id);
-		else
-			return "error";
-	}
+    public function update()
+    {
+        $id = Input::get('id');
+        $comic = Comic::find($id);
+        $comic->name = Input::get('name');
+        $comic->number = Input::get('number');
+        $comic->available = Input::get('available');
+        $new_price = Input::get('price');
+        if ($new_price != $comic->price) {
+            $comic->price = $new_price;
+            DB::update('update comic_user set price = ' . $new_price . ' where comic_id = ' . $id . ' and state_id < 3');
+        }
+        if (Input::get('active'))
+            $comic->active = 1;
+        else {
+            $comic->active = 0;
+            DB::update('update comic_user set active = 0 where comic_id = ' . $id);
+        }
+        $comic->save();
+        $return = Input::get('return');
 
-	public function remove() {
-		$id = Input::get('id');
-		$comic = ComicUser::where('comic_id', '=', $id);
-		$comic -> active = 0;
-		$comic -> update();
-		return Redirect::to('boxes/' . $comic -> series_id . '/' . $id);
-	}
+        if ($return == 'comics')
+            return Redirect::to('comics/' . $id);
+        elseif ($return == 'series')
+            return Redirect::to('series/' . $comic->series_id . '/' . $id);
+        else
+            return "error";
+    }
 
-	public function updateComicUser($series_id, $comic) {
-		$series_user = SeriesUser::whereRaw('series_id = ' . $series_id . ' and active = 1') -> get();
-		foreach ($series_user as $user) {
-			$comicUser = new ComicUser;
-			$comicUser -> comic_id = $comic -> id;
-			$comicUser -> user_id = $user -> user_id;
-			$comicUser -> price = $comic -> price;
-			$comicUser -> save();
-		}
-	}
+    public function remove()
+    {
+        $id = Input::get('id');
+        $comic = ComicUser::where('comic_id', '=', $id);
+        $comic->active = 0;
+        $comic->update();
+        return Redirect::to('boxes/' . $comic->series_id . '/' . $id);
+    }
 
-	public function listAllComics() {
-		$comics = Comic::all();
-		$this -> layout -> content = View::make('admin/manageComics', array('comics' => $comics));
-	}
+    public function updateComicUser($series_id, $comic)
+    {
+        $series_user = SeriesUser::whereRaw('series_id = ' . $series_id . ' and active = 1')->get();
+        foreach ($series_user as $user) {
+            $comicUser = new ComicUser;
+            $comicUser->comic_id = $comic->id;
+            $comicUser->user_id = $user->user_id;
+            $comicUser->price = $comic->price;
+            $comicUser->save();
+        }
+    }
 
-	public function showShipmentLoader() {
-		$comics = Comic::where('active', '=', '1');
-		$this -> layout -> content = View::make('admin/shipmentLoader', array('comics' => $comics));
-	}
+    public function listAllComics()
+    {
+        $comics = Comic::all();
+        $this->layout->content = View::make('admin/manageComics', array('comics' => $comics));
+    }
 
-	public function loadShipment() {
-		$rules = array('comic_id' => 'required|min:1|integer|exists:comics,id,active,1','amount' => 'required|min:1|integer');
-		$validator = Validator::make(Input::all(), $rules);
-		if ($validator -> fails()) {
-			$messages = $validator -> messages();
-			return Redirect::to('newShipment')->withErrors($validator);
-		} else {
-			$comic_id = Input::get('comic_id');
-			$amount = Input::get('amount');
-			$comic = Comic::find($comic_id);
-			$comic -> available += $amount;
-			$comic -> update();
-			return Redirect::to('newShipment');
-		}
-	}
+    public function showShipmentLoader()
+    {
+        $comics = Comic::where('active', '=', '1');
+        $this->layout->content = View::make('admin/shipmentLoader', array('comics' => $comics));
+    }
 
-	public function getNumberFromSeries(){
-		$this -> layout = null;
-		$series_id = Input::get('series_id');
-		$comics = Comic::whereRaw('series_id = '. $series_id . ' and active = 1')->get();
-		return $comics;
-	}
+    public function loadShipment()
+    {
+        $rules = array('comic_id' => 'required|min:1|integer|exists:comics,id,active,1', 'amount' => 'required|min:1|integer');
+        $validator = Validator::make(Input::all(), $rules);
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            return Redirect::to('newShipment')->withErrors($validator);
+        } else {
+            $comic_id = Input::get('comic_id');
+            $amount = Input::get('amount');
+            $comic = Comic::find($comic_id);
+            $comic->available += $amount;
+            $comic->update();
+            return Redirect::to('newShipment');
+        }
+    }
 
-	public function manageComic($comic_id) {
-		$comic = Comic::find($comic_id);
-		if ($comic != null)
-			$this -> layout -> content = View::make('admin/editComic', array('comic' => $comic,'path' => '../'));
-		else
-			return Redirect::to('comics/' . $comic_id);
-	}
+    public function getNumberFromSeries()
+    {
+        $this->layout = null;
+        $series_id = Input::get('series_id');
+        $comics = Comic::whereRaw('series_id = ' . $series_id . ' and active = 1')->get();
+        return $comics;
+    }
+
+    public function manageComic($comic_id)
+    {
+        $comic = Comic::find($comic_id);
+        if ($comic != null)
+            $this->layout->content = View::make('admin/editComic', array('comic' => $comic, 'path' => '../'));
+        else
+            return Redirect::to('comics/' . $comic_id);
+    }
 }
+
 ?>
