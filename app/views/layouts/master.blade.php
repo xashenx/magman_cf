72c8c1aba34fbead2eeeae7e44b937c0c68ec0b8
thomas <t.marchiori@futur3.it>
2015-01-10 21:30:20 +0100

    change title

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,10 +5,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>
+      Magman - Fumetti
       @if(isset($title))
-        {{ $title }}
-      @else
-        Titolo da inserire
+        - {{ $title }}
       @endif
     </title>
     <!-- BOOTSTRAP STYLES-->
