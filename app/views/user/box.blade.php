af86bfd8aa255672449e50aaed7b1e9f71aee5e6
Fabrizio Zeni <fabrizio.zeni@gmail.com>
2015-02-09 13:49:34 +0100

    fix on user homepage

--- app/views/user/box.blade.php
+++ app/views/user/box.blade.php
@@ -26,61 +26,64 @@
             @endif
           </strong>
         </div>
-        <div class="panel-body">
-          <table class="table table-striped table-bordered table-hover" id="dataTables-comics">
-            <thead>
-            <tr>
-              <th>Fumetto</th>
-              <th>Cover</th>
-              @if($user->show_price)
-                <th>Prezzo</th>
-              @endif
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($comics as $comic)
-              @if ($inv_state == 1)
-                @if ($comic->comic->available > 1)
-                  {{--*/ $tr = 'success'; $skip = 0 /*--}}
-                @else
-                  {{--*/ $tr = 'odd gradeX'; $skip = 0 /*--}}
+        @if(count($comics)>0)
+          <div class="panel-body">
+            <table class="table table-striped table-bordered table-hover" id="dataTables-comics">
+              <thead>
+              <tr>
+                <th>Fumetto</th>
+                <th>Cover</th>
+                @if($user->show_price)
+                  <th>Prezzo</th>
                 @endif
-              @else
-                @if ($comic->comic->arrived_at > date('Y-m-d',strtotime('-1 month')))
-                  {{--*/ $tr = 'success'; $skip = 0 /*--}}
-                @else
-                  @if ($comic->comic->state == 2)
-                    {{--*/ $tr = 'warning'; $skip = 0 /*--}}
+              </tr>
+              </thead>
+              <tbody>
+              @foreach ($comics as $comic)
+                @if ($inv_state == 1)
+                  @if ($comic->comic->available > 1)
+                    {{--*/ $tr = 'success'; $skip = 0 /*--}}
                   @else
-                    {{--*/ $tr = 'odd gradeX'; $skip = 1 /*--}}
+                    {{--*/ $tr = 'odd gradeX'; $skip = 0 /*--}}
+                  @endif
+                @else
+                  @if ($comic->comic->arrived_at > date('Y-m-d',strtotime('-1 month')))
+                    {{--*/ $tr = 'success'; $skip = 0 /*--}}
+                  @else
+                    @if ($comic->comic->state == 2)
+                      {{--*/ $tr = 'warning'; $skip = 0 /*--}}
+                    @else
+                      {{--*/ $tr = 'odd gradeX'; $skip = 1 /*--}}
+                    @endif
                   @endif
                 @endif
-              @endif
-              @if(!$skip)
-                <tr class="{{{ $tr }}}">
-                  <td>
-                    {{ $comic->comic->series->name}}
-                    {{{ ($comic->comic->series->version != null) ? ' - '.$comic->comic->series->version : '' }}}
-                    <strong>
-                      # {{ $comic->comic->number }}
-                    </strong>
-                  </td>
-                  <td>
-                    @if($comic->comic->image)
-                      <a href="{{$comic->comic->image}}" target="_blank"><img src="{{$comic->comic->image}}" alt="" height="42" width="42"></a>
-                    @endif
-                  </td>
-                  @if($user->show_price)
+                @if(!$skip)
+                  <tr class="{{{ $tr }}}">
                     <td>
-                      {{ round($comic->price,2) }} €
+                      {{ $comic->comic->series->name}}
+                      {{{ ($comic->comic->series->version != null) ? ' - '.$comic->comic->series->version : '' }}}
+                      <strong>
+                        # {{ $comic->comic->number }}
+                      </strong>
                     </td>
-                  @endif
-                  @endif
-                </tr>
-                @endforeach
-            </tbody>
-          </table>
-        </div>
+                    <td>
+                      @if($comic->comic->image)
+                        <a href="{{$comic->comic->image}}" target="_blank"><img src="{{$comic->comic->image}}" alt=""
+                                                                                height="42" width="42"></a>
+                      @endif
+                    </td>
+                    @if($user->show_price)
+                      <td>
+                        {{ round($comic->price,2) }} €
+                      </td>
+                    @endif
+                    @endif
+                  </tr>
+                  @endforeach
+              </tbody>
+            </table>
+          </div>
+        @endif
       </div>
     </div>
     @if(count($series)>0)
